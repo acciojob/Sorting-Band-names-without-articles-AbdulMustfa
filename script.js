a4e6be0c2ba8d1ab8660e5fdaa01d1cfd0bcaa35
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-14 22:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+def sort_bands(band_names):
+
+    articles = ['a', 'an', 'the']
+
+    strip_articles = lambda name: ' '.join(word for word in name.split() if word.lower() not in articles)
+
+    sorted_bands = sorted(band_names, key=strip_articles)
+
+    return sorted_bands
+band_names = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith', 'Anthrax']
+sorted_bands = sort_bands(band_names)
+print('<ul id="band">')
+for band in sorted_bands:
+    print(f'<li>{band}</li>')
+print('</ul>')
 
